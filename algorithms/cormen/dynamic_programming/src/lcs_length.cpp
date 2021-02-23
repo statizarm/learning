5ab2041508d2c02e69b1bb1e280554191b1ed24e
statizarm <statiz.arm@gmail.com>
2021-02-23 20:12:31 +0300

    max lcs length function finished

--- algorithms/cormen/dynamic_programming/src/lcs_length.cpp
+++ algorithms/cormen/dynamic_programming/src/lcs_length.cpp
@@ -4,40 +4,42 @@
 
 int lcs_length(const std::string &x, const std::string &y)
 {
-	std::vector<std::vector<int>> max_length;
+	std::vector<int> max_length;
 
 	int x_len = x.size();
 	int y_len = y.size();
 
-	max_length.resize(x_len + 1);
+	max_length.resize(y_len + 1);
 
-	for (auto &row : max_length) {
-		row.resize(y_len + 1);
-		row[0] = 0;
-	}
-	for (auto &elem : max_length[0]) {
+	for (auto &elem : max_length) {
 		elem = 0;
 	}
 
 	const char *x_raw = x.c_str() - 1;
 	const char *y_raw = y.c_str() - 1;
+
 	for (int i = 1; i <= x_len; ++i) {
+		int prev = max_length[0];
 		for (int j = 1; j <= y_len; ++j) {
 			int max_val;
 
 			if (x_raw[i] == y_raw[j]) {
-				max_val = max_length[i-1][j-1] + 1;
-			} else if ((max_val = max_length[i][j-1]) <
-				max_length[i-1][j]) {
+				max_val = prev + 1;
+			} else {
+				max_val = max_length[j];
+				int alt_val = max_length[j - 1];
 
-				max_val = max_length[i-1][j];
+				if (max_val < alt_val) {
+					max_val = alt_val;
+				}	
 			}
 
-			max_length[i][j] = max_val;
+			prev = max_length[j];
+			max_length[j] = max_val;
 		}
 	}
 
-	return max_length[x_len][y_len];
+	return max_length[y_len];
 }
 
 int main(int argc, char *argv[])
